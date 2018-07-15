54add68d267f0e318d64060e98cd6dd92d8dfbe5
celia <tcc_2007@126.com>
2018-07-15 15:30:03 +0800

    导航

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
+
+// 移动设备上的浏览器默认会在用户点击屏幕大约延迟300毫秒后才会触发点击事件，这是为了检查用户是否在做双击。为了能够立即响应用户的点击事件，才有了FastClick。
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
